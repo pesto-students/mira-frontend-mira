975ef24b2b8e553b5a81ee57eae7d2ce64d0a38f
m-bohra8999 <murtaza.bohra8999@gmail.com>
2023-03-12 19:23:47 +0530

    add homepage, non-functional login and register comp and customize theme

---
+++ src/App/theme/ThemeProvider.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material';
+import { StylesProvider } from '@mui/styles';
+import theme from './theme';
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+const ThemeProviderWrapper: React.FC<IProps> = ({ children }) => {
+  return (
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </StylesProvider>
+  );
+};
+
+export default ThemeProviderWrapper;
