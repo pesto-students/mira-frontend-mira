906cc1dc43488f4fe0c39b0707528352953a7d88
boaloysius <boaloysius@gmail.com>
2023-04-13 01:29:05 +0530

    remove conflicting package

--- src/App/theme/ThemeProvider.tsx
+++ src/App/theme/ThemeProvider.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/';
-import { StylesProvider } from '@mui/styles';
 import theme from './theme';
 
 interface IProps {
@@ -8,11 +7,7 @@ interface IProps {
 }
 
 const ThemeProviderWrapper: React.FC<IProps> = ({ children }) => {
-  return (
-    <StylesProvider injectFirst>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </StylesProvider>
-  );
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
 
 export default ThemeProviderWrapper;
