388d1a06deb62cd7ce46a22ebd25ad360f0333f4
boaloysius <boaloysius@gmail.com>
2023-04-14 16:28:39 +0530

    add project landing page and default project route

---
+++ src/pages/project/ProjectDefault.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Image from 'App/assets/img.png';
+import { Box, Typography, styled } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import { useAppSelector } from 'App/hooks';
+
+const StyledLink = styled(NavLink)(({ theme }) => ({
+  textDecoration: 'none',
+  '&:focus,&:hover,&:visited,&:link,&:active': {
+    textDecoration: 'none',
+  },
+  color: theme.colors.primary,
+}));
+
+export default function ProjectDefault() {
+  const { currentProject } = useAppSelector((state) => state.project);
+  return (
+    <Box
+      sx={() => ({
+        '&.MuiBox-root': {
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        },
+      })}
+      component="div"
+    >
+      <img src={Image} alt="logo" />
+      <Typography
+        sx={{ maxWidth: '600px', padding: '10px', marginTop: '20px' }}
+      >
+        Mira let you <StyledLink to="/projects/create">create</StyledLink> and{' '}
+        <StyledLink to={`/projects/${currentProject._id}/overview`}>
+          manage
+        </StyledLink>{' '}
+        projects, very easy. Now you can create issues as{' '}
+        <StyledLink to={`/projects/${currentProject._id}/cards/create`}>
+          cards
+        </StyledLink>{' '}
+        and manage them using{' '}
+        <StyledLink to={`/projects/${currentProject._id}/dashboard`}>
+          interactive dashboard
+        </StyledLink>
+        .
+      </Typography>
+    </Box>
+  );
+}
